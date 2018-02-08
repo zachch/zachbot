306c954d92a33579e73523e49830956570991692
Glitch (amused-quilt) <none>
2018-02-08 22:13:45 +0800

    Add second question

--- skills/start_sequence.js
+++ skills/start_sequence.js
@@ -11,46 +11,67 @@ module.exports = function(controller) {
 
     bot.createConversation(message, function(err, convo) {
 
-      // create a path for when a user says YES
+      // Q1. Have we met before question
       convo.addMessage({
         text: 'Excuuuseee me. You definitely didn\'t say yes =/. Again!',
-      },'yes_thread');
+        action: 'default',
+      },'met_before_yes_thread');
 
-      // create a path for when a user says NO
-      // mark the conversation as unsuccessful at the end
-      convo.addMessage({
-        text: '',
-        action: 'stop', // this marks the conversation as unsuccessful
-      },'no_thread');
-
-      // create a path where neither option was matched
-      // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
       convo.addMessage({
         text: 'Ehhhh stick to the script la. Try again!',
         action: 'default',
-      },'bad_response');
+      },'met_before_bad_response');
 
-      // Create a yes/no question in the default thread...
       convo.addQuestion('Heyy... Have we met before?', [
         {
           pattern:  bot.utterances.yes,
           callback: function(response, convo) {
-            convo.gotoThread('yes_thread');
-          },
-        },
-        {
-          pattern:  bot.utterances.no,
-          callback: function(response, convo) {
-            convo.gotoThread('no_thread');
+            convo.gotoThread('met_before_yes_thread');
           },
         },
         {
           default: true,
           callback: function(response, convo) {
-            convo.gotoThread('bad_response');
+            convo.gotoThread('met_before_bad_response');
+          },
+        },
+        {
+          pattern:  bot.utterances.no,
+          callback: function(response, convo) {
+            convo.gotoThread('hannah_thread');
           },
         }
       ], {}, 'default');
+
+      // Q2. Hannah's friend question
+      convo.addMessage({
+        text: 'Oh...',
+      },'hannah_thread');
+
+      convo.addMessage({
+        text: 'YESSSS. Ka Ching!',
+        action: 'completed',
+      },'hannah_yes_thread');
+
+      convo.addMessage({
+        text: 'Wrong answer la',
+        action: 'hannah_thread',
+      },'hannah_bad_response');
+
+      convo.addQuestion('You are Hannah\'s friend?', [
+        {
+          pattern:  bot.utterances.yes,
+          callback: function(response, convo) {
+            convo.gotoThread('hannah_yes_thread');
+         },
+        {
+          default: true,
+          callback: function(response, convo) {
+            convo.gotoThread('hannah_bad_response');
+          },
+        }
+      ], {},'hannah_thread');
+
 
       convo.activate();
 
@@ -58,6 +79,7 @@ module.exports = function(controller) {
       convo.on('end', function(convo) {
 
         convo.say(convo.status);
+        convo.say('THE END');
         if (convo.successful()) {
           // this still works to send individual replies...
           bot.reply(message, 'Let us eat some!');
