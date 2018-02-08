91d17d538090dad926b954d4f07f3c0d6f1f7e9a
Glitch (amused-quilt) <none>
2018-02-08 21:15:14 +0800

    Start sequence fleshing out

--- skills/start_sequence.js
+++ skills/start_sequence.js
@@ -9,7 +9,31 @@ module.exports = function(controller) {
   controller.api.messenger_profile.greeting('');
   controller.on('facebook_postback', function(bot, message) {
 
-    bot.reply(message, 'Heyy');
+    bot.startConversation(message, function(err, convo) {
+      convo.say(message, 'Heyy');
+      convo.ask(message, 'Have we met before?', [
+        {
+          pattern:  bot.utterances.yes,
+          callback: function(response, convo) {
+            convo.say(message, 'That\'s not what you said! Try again');
+          },
+        },
+        {
+          pattern:  bot.utterances.no,
+          callback: function(response, convo) {
+            convo.say(message, 'Umm you are Hannah\'s friend right?');
+          },
+        },
+        {
+          default: true,
+          callback: function(response, convo) {
+            convo.say(message, 'That\'s not what you said! Try again');
+            convo.gotoThread('yes_thread');
+          },
+        }
+      ]
+      );
+    });
 
   });
 
